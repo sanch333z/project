f6157f552e0812e1ff996df78822a9c800dcbb37
Alex Fedorov <sanch333z@gmail.com>
2016-08-06 13:55:34 +0300

    new version server.js

--- myserv/server.js
+++ myserv/server.js
@@ -6,13 +6,13 @@ app.get('/', function (req, res) {
   res.status(200).send('ok');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-//var server = app.listen(3000, function () {
-//  var port = server.address().port;
-//  console.log('Example app listening at port %s', port);
+//app.listen(3000, function () {
+//  console.log('Example app listening on port 3000!');
 //});
 
-//module.exports = server;
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Example app listening at port %s', port);
+});
+
+module.exports = server;
